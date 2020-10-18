23d0d5a45285e8f5cbf8f964c019129ce1e95141
Piotr Trochymiak <Piotr@Trochymiak.net>
2020-10-18 21:55:38 +0200

    go to github

---
+++ index.js
@@ -0,0 +1,35 @@
+
+(async function () {
+
+	var url = 'https://newsapi.org/v2/top-headlines?country=pl&apiKey=e3b79232803745b7b6ba37f0d994dd71';
+	//var url = 'news.json';
+	var data = await (await fetch(url)).json();
+	//console.log(data.articles[0]);
+	data.articles.forEach(art => {
+
+		var head = document.createElement("div");
+		head.style.backgroundImage = `url(${art.urlToImage})`;
+		head.classList.add('head');
+
+		var aElem = document.createElement('a');
+		aElem.setAttribute("href", art.url);
+
+		var title = document.createElement('div');
+		title.classList.add('title');
+		title.innerText = art.title;
+		head.appendChild(title);
+
+		var desc = document.createElement('div');
+		desc.classList.add('desc');
+		desc.innerHTML = art.description;
+
+		aElem.appendChild(head);
+		aElem.appendChild(desc);
+
+		//anchorElem.innerHTML = desc;
+		//document.body.appendChild(head);
+		document.body.appendChild(aElem);
+
+	})
+
+})();
---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'sw';
+const urlsToCache = [
+	'index.html',
+	'index.js,',
+	'index.css'
+];
+self.addEventListener('install', (event) => {
+	event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+		return cache.addAll(urlsToCache);
+	}));
+});
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(caches.match(event.request).then((response) => {
+		return response ? response : fetch(event.request);
+	}));
+});
